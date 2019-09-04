b3cb811c64baf684da8b613f1886b91071e6d0e5
Bruno Sendras <brunosendras@gmail.com>
2019-09-04 13:23:05 -0300

    Initial Commit

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import Hello from '../utils/hello';
+
+/**
+ * Este es un proyecto vacio de node js, configurado para compilar
+ * con babel 7 las ultimas caracteristicas de Ecma Script 6.
+ */
+
+export default function App() {
+    /**
+     * Comienza a codear aquí
+     */
+
+    Hello.world();
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from './app';
+
+/**
+ * Punto de ejecución de la app
+ */
+App();
